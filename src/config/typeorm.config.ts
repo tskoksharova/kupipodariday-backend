30603d01378ad8be97ddf58b3ccc541d87cc0053
tskoksharova <tskoksharova@gmail.com>
2026-02-11 23:54:04 +0500

    Итерация_1

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,16 +1,22 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeormConfig = (): TypeOrmModuleOptions => ({
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: Number(process.env.POSTGRES_PORT) || 5432,
-  username: process.env.POSTGRES_USER || 'student',
-  password: process.env.POSTGRES_PASSWORD || '',
-  database: process.env.POSTGRES_DB || 'kupipodariday',
+export const typeormConfig = (): TypeOrmModuleOptions => {
+  console.log('DB CONNECT TO =>', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    db: process.env.POSTGRES_DB,
+  });
 
-  synchronize: true,
-
-  autoLoadEntities: true,
-
-  logging: false,
-});
+  return {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: Number(process.env.POSTGRES_PORT) || 5432,
+    username: process.env.POSTGRES_USER || 'student',
+    password: process.env.POSTGRES_PASSWORD || '03101985',
+    database: process.env.POSTGRES_DB || 'kupipodariday',
+    synchronize: false,
+    autoLoadEntities: true,
+    logging: false,
+  };
+};
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log('POSTGRES_DB=', process.env.POSTGRES_DB);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: true,
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,7 +16,7 @@ async function bootstrap() {
     }),
   );
 
-  const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
-  await app.listen(PORT, '0.0.0.0');
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port);
 }
 bootstrap();
